9004fa08c7aa7f3c504758f24db319ad7736eecb
yamadaj2 <jonathon.yamada@gmail.com>
2021-01-02 00:54:20 +0000

    added id and click listener. Removed analytics

--- _site/assets/js/index.js
+++ _site/assets/js/index.js
@@ -1,1 +1,9 @@
-import '../js/*'
+(() => {
+  const linkContainerItems = document.getElementsByClassName('link-container');
+
+  if (linkContainerItems?.length > 0) {
+    for (const link of linkContainerItems) {
+      link.addEventListener('click', ({currentTarget: {id}}) => console.log(id));
+    }
+  }
+})()

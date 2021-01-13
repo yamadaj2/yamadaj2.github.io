f310f5eb77d23f339d480f2a7bd3b3358906f3b1
yamadaj2 <jonathon.yamada@gmail.com>
2021-01-13 22:08:32 +0000

    fixed bug

--- _site/assets/js/index.js
+++ _site/assets/js/index.js
@@ -29,7 +29,7 @@
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        analytics: {
+        landing_page_analytic: {
           is_mobile: window?.navigator?.userAgentData?.mobile,
           referrer: document.referrer
         }

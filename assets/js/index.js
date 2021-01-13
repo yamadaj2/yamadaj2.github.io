1ff300db5c63374a75967848e2ea21dfc5bbb83e
yamadaj2 <jonathon.yamada@gmail.com>
2021-01-13 21:58:53 +0000

    added landing page analytics

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,42 @@
 (() => {
-  const linkContainerItems = document.getElementsByClassName('link-container');
-  if (linkContainerItems?.length > 0) {
-    for (const link of linkContainerItems) {
-      link.addEventListener('click', () => {
-        fetch('https://jy-makes-music-api.herokuapp.com/analytics', {
-        // fetch('http://localhost:3001/analytics', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            analytics: {
-              click_type: link?.id,
-              is_mobile: window?.navigator?.userAgentData?.mobile,
-              referrer: document.referrer
-            }
-          }),
-        })
-      });
+  const listenForLinkClick = () => {
+    const linkContainerItems = document.getElementsByClassName('link-container');
+    if (linkContainerItems?.length > 0) {
+      for (const link of linkContainerItems) {
+        link.addEventListener('click', () => {
+          fetch('https://jy-makes-music-api.herokuapp.com/analytics', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              analytics: {
+                click_type: link?.id,
+                is_mobile: window?.navigator?.userAgentData?.mobile,
+                referrer: document.referrer
+              }
+            }),
+          })
+        });
+      }
     }
   }
+
+  const listingForProfileLoad = () => {
+    fetch('https://jy-makes-music-api.herokuapp.com/landing_page_analytics', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        analytics: {
+          is_mobile: window?.navigator?.userAgentData?.mobile,
+          referrer: document.referrer
+        }
+      }),
+    })
+  }
+
+  listenForLinkClick();
+  listingForProfileLoad()
 })()
